ef2f3a05c09c04da7332fd79ff8ef031516b5ded
markoboras0712 <mboras1@etfos.hr>
2021-05-04 17:36:01 +0300

    plugin-date filter and core components plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -37,6 +37,8 @@ module.exports = {
   ** Plugins to load before mounting the App
   */
   plugins: [
+    '~plugins/date-filter.js',
+    '~plugins/core-components.js'
   ],
 
   /*
--- pages/admin/auth/index.vue
+++ pages/admin/auth/index.vue
@@ -13,15 +13,9 @@
 </template>
 
 <script>
-import AppControlInput from '../../../components/UI/AppControlInput.vue';
-import AppButton from '../../../components/UI/AppButton.vue';
 export default {
     name:'AdminAuthPage',
     layout:'admin',
-    components:{
-        AppControlInput,
-        AppButton
-    },
     data(){
         return{
             isLogin:true
